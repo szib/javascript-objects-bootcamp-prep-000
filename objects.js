69ccd73480cf939fddf0ff818b5ff746aa7d1700
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 22:07:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,16 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle})
 }
 
-// function removeFromPlaylist(playlist, artistName) {
-//   delete playlist[artistName]
-//   return playlist
-// }
-
-
 function removeFromPlaylist(playlist, artistName) {
-  var pl = Object.assign({}, playlist)
-  console.log(pl)
-  delete pl[artistName]
-    console.log(pl)
-  return pl
+  delete playlist[artistName]
+  return playlist
 }
