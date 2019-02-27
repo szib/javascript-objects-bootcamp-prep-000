494f48cb6576f8afe93bf6b800497c5418be1a62
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 22:03:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,9 @@ var playlist = { 'World Order': 'Missing beauty' }
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  var pl = Object.assign({}, playlist)
+  delete pl[artistName]
+  return pl
+}
